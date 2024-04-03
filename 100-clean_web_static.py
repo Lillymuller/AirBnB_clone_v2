298a37c2826d6f02d5e9722f1faa53578374fde4
Lillymuller <eleltaalemu@gmail.com>
2024-04-03 10:48:24 -0700

    update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,7 +11,8 @@ def do_clean(number=0):
     """Deletes out-of-date archives on local and remote servers
 
     Args:
-    number (int): The number of archives to keep (default: 0, keeps only the most recent)
+    number (int): The number of archives to keep
+    (default: 0, keeps only the most recent)
 
     Raises:
     ValueError: If the provided number is negative
@@ -27,7 +28,8 @@ def do_clean(number=0):
 """Delete remote archives (assuming sudo access)"""
 for server in env.hosts:
     with sudo(server):
-        remote_archives = run("ls -tr /data/web_static/releases/ | grep 'web_static_'").split()
+        remote_archives = run("ls -tr /data/web_static/releases/ | grep
+                'web_static_'").split()
         remote_archives = remote_archives[::-1][:number]
         for archive in set(remote_archives):
             run("rm -rf /data/web_static/releases/{}".format(archive))
