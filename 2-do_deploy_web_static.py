45f172f92f37c27efbe8225f421e9c0817075e47
Lillymuller <eleltaalemu@gmail.com>
2024-04-03 10:29:55 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["3.86.7.100", "100.26.212.112"]
 
 
 def do_deploy(archive_path):
@@ -13,9 +13,6 @@ def do_deploy(archive_path):
 
     Args:
         archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
         return False
