dcf0932a1bca9c6473a2d8ea4e92fd069bf08d61
Lillymuller <eleltaalemu@gmail.com>
2024-04-03 09:16:26 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,54 @@
-#!/usr/bin/python3
-"""
-Fabric script (1-pack_web_static.py) that distributes an archive
-To your web servers, using the function do_deploy
-"""
-from fabric.api import env, put, sudo, run
-import os.path
-
-env.hosts = ["3.86.7.100", "100.26.212.112"]
+import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
 
 
-def do_deploy(archive_path):
-    """
-    Deploys the archive to web servers and updates the configuration
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
-    try:
-        filename = archive_path.split("/")[-1]
-        no_ext = filename.split(".")[0]
-        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
-        symlink = "/data/web_static/current"
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
-        run("rm -rf {}web_static".format(path_no_ext))
-        run("rm -rf {}".format(symlink))
-        run("ln -s {} {}".format(path_no_ext, symlink))
-        return True
-    except:
-        return False
+class Deployer:
+    def __init__(self, hosts):
+        self.hosts = hosts
+
+    def create_archive(self):
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = f"versions/web_static_{timestamp}.tgz"
+
+        try:
+            print("Packing web_static to {}".format(archive_path))
+            local(f"tar -cvzf {archive_path} web_static")
+            archive_size = os.stat(archive_path).st_size
+            print(f"web_static packed: {archive_path} -> {archive_size} Bytes")
+        except Exception as e:
+            print(f"Error creating archive: {e}")
+            archive_path = None
+
+        return archive_path
+
+    @runs_once
+    def deploy_archive(self, archive_path):
+        if not os.path.exists(archive_path):
+            return False
+
+        file_name = os.path.basename(archive_path)
+        folder_name = file_name.replace(".tgz", "")
+        folder_path = f"/data/web_static/releases/{folder_name}/"
+
+        try:
+            put(archive_path, f"/tmp/{file_name}")
+            run(f"mkdir -p {folder_path}")
+            run(f"tar -xzf /tmp/{file_name} -C {folder_path}")
+            run(f"rm -rf /tmp/{file_name}")
+            run(f"mv {folder_path}web_static/* {folder_path}")
+            run(f"rm -rf {folder_path}web_static")
+            run(f"rm -rf /data/web_static/current")
+            run(f"ln -s {folder_path} /data/web_static/current")
+            print("New version deployed!")
+            return True
+        except Exception as e:
+            print(f"Error during deployment: {e}")
+            return False
+
+
+if __name__ == "__main__":
+    deployer = Deployer(hosts=['3.86.7.100', '100.26.212.112'])
+    archive_path = deployer.create_archive()
+    if archive_path:
+        deployer.deploy_archive(archive_path)
