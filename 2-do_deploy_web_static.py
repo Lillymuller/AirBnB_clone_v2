4a22cbdc35eb756ebc6ab06aaa4b0bde25160128
Lillymuller <eleltaalemu@gmail.com>
2024-04-03 22:35:32 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,22 +8,8 @@ env.user = 'ubuntu'
 env.hosts = ['3.86.7.100', '100.26.212.112']
 
 
-def do_pack():
-    """Generates a .tgz archive of the web_static folder returns its path."""
-    timestamp = time.strftime("%Y%m%d%H%M%S")
-    archive_path = f"versions/web_static_{timestamp}.tgz"
-
-    try:
-        with lcd("web_static"):
-            local("mkdir -p ../versions")
-            local(f"tar -cvzf ../{archive_path} .")
-            return archive_path
-    except:
-        return None
-
-
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
+    """
 
     Args:
         archive_path (str): The path of the archive to distribute.
