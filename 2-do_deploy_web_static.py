92819eb0b28846cc3d03106d3ef6faa617d132df
Lillymuller <eleltaalemu@gmail.com>
2024-04-03 22:28:34 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,33 +9,37 @@ env.hosts = ['3.86.7.100', '100.26.212.112']
 
 
 def do_pack():
-    """Packes web_static in tgz format"""
-    t = datetime.now()
-    name = "web_static_{}{}{}{}{}{}.tgz".format(t.year, t.month, t.day, t.hour,
-            t.minute, t.second)
-    local('mkdir -p versions')
-    local("tar -cvzf versions/{} web_static".format(name))
-    size = os.stat("versions/{}".format(name)).st_size
-    print("web_static packed: versions/{} -> {}".format(name, size))
+    """Generates a .tgz archive of the web_static folder returns its path."""
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    archive_path = f"versions/web_static_{timestamp}.tgz"
+
+    try:
+        with lcd("web_static"):
+            local("mkdir -p ../versions")
+            local(f"tar -cvzf ../{archive_path} .")
+            return archive_path
+    except:
+        return None
+
 
 def do_deploy(archive_path):
     if not archive_path:
-        eturn(False)
-        name = archive_path.split('/')[1]
+        return(False)
+    name = archive_path.split('/')[1]
 
-        try:
-            put(archive_path, '/tmp/')
-            run("mkdir -p /data/web_static/releases/{}".format(name))
-            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-                    .format(name, name))
-            run("rm /tmp/{}".format(name))
-            run("mv /data/web_static/releases/{}/web_static/*\
-                    /data/web_static/releases/{}".format(name, name))
-            run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-            run("rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-                    .format(name))
-            print("New version deployed")
-            return(True)
-        except BaseException:
-            return(False)
+    try:
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+                .format(name, name))
+        run("rm /tmp/{}".format(name))
+        run("mv /data/web_static/releases/{}/web_static/*\
+                /data/web_static/releases/{}".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+                .format(name))
+        print("New version deployed")
+        return(True)
+    except BaseException:
+        return(False)
