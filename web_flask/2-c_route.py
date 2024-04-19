8d378ed0497749943e5427eb631fb80e0fec6eb8
Lillymuller <eleltaalemu@gmail.com>
2024-04-19 07:30:36 -0700

    C route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,29 +4,28 @@ script that starts a Flask web application
 Your web application must be listening on 0.0.0.0, port 5000
 Routes /Hello HBNB, /hbnb and /c/<text>
 """
-
-form flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Display Hello HBNB"""
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Display hbnb"""
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Display “C ” followed by the value of the text variable"""
-    text = text.replace('_', ' ')
-    return 'C' + text
+    """Displays 'C' followed by the value of the text variable"""
+    text = text.replace("_", " ")
+    return "C" + text
 
 
 if __name__ == '__main__':
