f48c18cfab4f7b745ec8e2e080f540478a26e76f
Lillymuller <eleltaalemu@gmail.com>
2024-04-19 08:51:09 -0700

    odd or even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -54,7 +54,7 @@ def num_template(n):
     return render_template("5-number.html", n=n)
 
 
-@app.rute("/number_odd_or_even/<int:n>", strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def odd_even(n):
     """Display a HTML page only if n is an integer"""
     return render_template("6-number_odd_or_even.html", n=n)
