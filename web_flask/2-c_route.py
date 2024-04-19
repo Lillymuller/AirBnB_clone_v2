b84f38520c0ad36d045598a18a1aa655019b29d5
Lillymuller <eleltaalemu@gmail.com>
2024-04-19 07:19:34 -0700

    C route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -25,8 +25,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Display “C ” followed by the value of the text variable"""
-    show = f"C {text.replace('_', ' ')}"
-    print(show)
+    return f"C {text.replace('_', ' ')}"
 
 
 if __name__ == '__main__':
