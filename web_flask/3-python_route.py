ece08064822a9120de5525b1302f90fbe6a2437a
Lillymuller <eleltaalemu@gmail.com>
2024-04-19 07:47:44 -0700

    python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -32,7 +32,7 @@ def c(text):
 @app.route("/python/<text>", strict_slashes=False)
 def pythoncool(text='is cool'):
     """Display “Python ”, followed by the value of the text variable"""
-    return "python {}".format(text.replace("_", " "))
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
