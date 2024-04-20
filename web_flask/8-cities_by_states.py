833b55bc902d0d3854e5c9fcc6cc6b1a7cc358ae
Lillymuller <eleltaalemu@gmail.com>
2024-04-20 02:00:15 -0700

    cities_by_states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,6 +10,9 @@ from flask import Flask, render_template
 from models import *
 from models import storage
 
+app = Flask(__name__)
+
+
 @app.route("/cities_by_states",  strict_slashes=False)
 def cities_states():
     """Displays an HTML page with list of all State in DBStorage"""
@@ -25,4 +28,3 @@ def teardown(exception):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
-
